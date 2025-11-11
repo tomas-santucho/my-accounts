39e9bcd5159390dfce7c919e0d98613acf167008
DeschTech <thomas@deschtech.com>
2025-11-11 20:49:48 -0300

    Added Rate Limiter

--- src/infrastructure/http/expressServer.ts
+++ src/infrastructure/http/expressServer.ts
@@ -1,18 +1,20 @@
-// src/infrastructure/http/expressServer.ts
 import express from "express";
 import pinoHttp from "pino-http";
 import {logger} from "./logger/logger";
+import {globalRateLimiter} from "./rate-limiter/rateLimiter";
 
 export const startServer = () => {
     const app = express();
 
     app.use(pinoHttp({ logger }));
+    app.use(express.json());
+    app.use(globalRateLimiter);
 
     app.get("/", (req, res) => {
         req.log.info("Root route hit");
         res.json({ msg: "Hello from Pino!" });
     });
 
-    const port = process.env["PORT"] || 3000;
+    const port = process.env["PORT"] || 3001;
     app.listen(port, () => logger.info(`🚀 Server running on port ${port}`));
 };
