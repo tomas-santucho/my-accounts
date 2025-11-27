bf2260301d397203df6b30dd806501e7ab427954
Tomas Santucho <voidsynth@proton.me>
2025-11-27 19:18:59 -0300

    Fix Import

--- lambda.js
+++ lambda.js
@@ -1,12 +1,8 @@
 const serverless = require("serverless-http");
-const { getApp } = require("./dist/app");
+import {getApp} from "./src/app.js";
 
 let handler;
 
-/**
- * AWS Lambda handler
- * Initializes Express app on cold start and reuses on warm starts
- */
 module.exports.handler = async (event, context) => {
     if (!handler) {
         const app = await getApp();
